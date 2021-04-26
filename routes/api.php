98210a871e5b15a9e66e66caa6af682429c3018a
Gustavo Bravo <gbravo@lyracons.com>
2021-04-26 20:16:56 -0300

    First commit. JWT config. User seeder

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        // Below mention routes are public, user can access those without any restriction.
+        // Create New User
+        Route::post('register', [AuthController::class, 'register']);
+        // Login User
+        Route::post('login', [AuthController::class, 'login']);
+
+
+        // Below mention routes are available only for the authenticated users.
+        Route::middleware('auth:api')->group(function () {
+            // Get user info
+            Route::get('user', [AuthController::class, 'user']);
+            // Logout user from application
+            Route::post('logout', [AuthController::class, 'logout']);
+        });
+    });
+});
+
